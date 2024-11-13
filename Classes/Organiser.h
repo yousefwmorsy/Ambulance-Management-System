89f3d5043c1ab11a4c1c3d91495e1f42b481af80
Yousef Morsy <yousefwmorsy@gmail.com>
2024-11-13 20:45:55 +0200

    added car and request classes + organiser data members

--- Classes/Organiser.h
+++ Classes/Organiser.h
@@ -1,5 +1,14 @@
 #pragma once
+#include "..//Data Structures//Queue//LinkedQueue.h"
+#include "Car.h"
+#include "Request.h"
+
 class Organiser
 {
+private:
+	int timestep = 0;
+	LinkedQueue<Car> OutCars;
+	LinkedQueue<Car> BackCars;
+	LinkedQueue<Request> FinishList;
 };
 
