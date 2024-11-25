e5bef6ff4261de2212ac178fb238406a291f47cb
Yousef Morsy <yousefwmorsy@gmail.com>
2024-11-25 23:42:26 +0200

    #

--- Data Structures/LeavablePriQueue/LeavablePriQueue.cpp
+++ Data Structures/LeavablePriQueue/LeavablePriQueue.cpp
@@ -1,15 +1,15 @@
 #include "LeavablePriQueue.h"
 
-void LeavablePriQueue::LeaveQueue(int x, int PID)
+void LeavablePriQueue::LeaveQueue(Car* &Removed, int PID)
 {
-	priNode<Car>* ptr = head; 
-	while (ptr && ptr->getNext()->getItem(x).getPID() != PID) {
+	priNode<Car*>* ptr = head; 
+	int x;
+	while (ptr && ptr->getNext()->getItem(x)->getPID() != PID) {
 		ptr = ptr->getNext();
 	} //traversal until it reaches null or ptr before the one to be deleted
 	if (ptr) {
-		priNode<Car>* temp = ptr->getNext(); //sets temp to ptr to be deleted
-		ptr->setNext(temp->getNext()); //sets the next of ptr to the next of deleted 
-		delete temp; 
-		temp = nullptr;
+		Removed = ptr->getNext()->getItem(x); //sets temp to ptr to be deleted
+		ptr->setNext(ptr->getNext()->getNext()); //sets the next of ptr to the next of deleted 
+	
 	}
 }
