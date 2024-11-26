b1c23d1711071ed0c71d9a9fbc2e0c40d06b4fed
omar27105 <152551143+omar27105@users.noreply.github.com>
2024-11-26 14:22:05 +0200

    Update Car.h

--- Classes/Car.h
+++ Classes/Car.h
@@ -10,12 +10,12 @@ private:
 	int carID;
 	int hospitalID;
 	bool isSpecial;
-	int patient_ID = -1; //negative one is when there is no patient in the car 
+	Request* ptr; //pointer for the patient	
 public:
 	Car(){}
 	Car(int hospitalID,int carID,bool isSpecial);
 	static void setSpeed(int speed_special, int speed_normal);
-	void setPID(int ID);
-	int getPID();       //I don't think we will use it but I am leaving it incase it was needed for debugging
+	void setPatient(Request *);
+	Request* getPatient();
 	friend ostream& operator << (ostream& out, const Car& c);
 };
