58ad884cdaad996ca5f0620febaea9c4daa122df
yousefwmorsy <166176410+yousefwmorsy@users.noreply.github.com>
2024-12-12 20:00:46 +0200

    Added Phase 1.2 Files After Final Edits

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Classes/Organiser.h"
+using namespace std;
+
+int main() {
+	Organiser* pOrg = new Organiser(); 
+
+	pOrg->Simulation();
+	
+	delete pOrg;
+	while (true) {}
+}
